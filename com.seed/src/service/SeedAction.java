82e0324c563eacc2d413a2078bf0c7a8c6086852
USER-COMPUTER\user <fun906@gmail.com>
2021-04-30 00:11:27 +0900

    12

--- com.seed/src/service/SeedAction.java
+++ com.seed/src/service/SeedAction.java
@@ -6,6 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface SeedAction {
 
-	public SeedActionForward execute(HttpServletRequest request , HttpServletResponse response) throws Exception;
+	public SeedActionForward execute(HttpServletRequest request ,HttpServletResponse response) throws Exception;
 	
 }
